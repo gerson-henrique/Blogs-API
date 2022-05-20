c9026dcd5bb470011a8c97358f5628e0141e17bb
Gero <gersonhoa@gmail.com>
2022-05-20 16:49:35 -0300

    feat : +user model

---
+++ src/database/models/User.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false, // remove a obrigatoriedade de utilizar os campos `createdAt` e `updatedAt`
+    tableName: 'Users',
+  });
+
+
+  return User;
+};
