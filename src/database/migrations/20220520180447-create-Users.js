ee780a136510b1e81e9a48c8cf639cea700242f3
Gero <gersonhoa@gmail.com>
2022-05-20 15:57:41 -0300

    feat : create all migrations

---
+++ src/database/migrations/20220520180447-create-Users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      displayName: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        unique:true,
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('Users');
+  },
+};
